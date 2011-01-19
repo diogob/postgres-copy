75684a85efb1cd79aa9c4697d8e7ee5f16892d7d
Diogo Biazus <diogob@gmail.com>
2011-01-19 17:04:02 -0200

    changed hard-coded quotes for snitize method

--- lib/activerecord-postgres-copy/base.rb
+++ lib/activerecord-postgres-copy/base.rb
@@ -3,7 +3,7 @@ module ActiveRecord
     def self.pg_copy_to path = nil
       if path
         raise "You have to choose between exporting to a file or receiving the lines inside a block" if block_given?
-        connection.execute "COPY (#{self.scoped.to_sql}) TO '#{path}' WITH DELIMITER '\t' CSV HEADER"
+        connection.execute "COPY (#{self.scoped.to_sql}) TO #{sanitize(path)} WITH DELIMITER '\t' CSV HEADER"
       else
         connection.execute "COPY (#{self.scoped.to_sql}) TO STDOUT WITH DELIMITER '\t' CSV HEADER"
         while line = connection.raw_connection.get_copy_data do
@@ -14,7 +14,7 @@ module ActiveRecord
     end
     def self.pg_copy_from path_or_io, field_map = nil
       if path_or_io.instance_of? String
-        connection.execute "COPY #{quoted_table_name} FROM '#{path_or_io}' WITH DELIMITER '\t' CSV HEADER"
+        connection.execute "COPY #{quoted_table_name} FROM #{sanitize(path_or_io)} WITH DELIMITER '\t' CSV HEADER"
       end
     end
   end
