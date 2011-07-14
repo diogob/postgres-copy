75c559857975dbe0845b5619a66bb06389cbabf5
Diogo Biazus <diogob@gmail.com>
2011-07-14 13:38:14 -0300

    changed default file name to a more generic one

--- lib/postgres-copy/csv_responder.rb
+++ lib/postgres-copy/csv_responder.rb
@@ -1,6 +1,6 @@
 class ActionController::Responder
   def to_csv
-    name = "#{controller.current_client.id}_#{Time.now.to_i}"
+    name = "#{controller.resource.class.name.downcase}_#{Time.now.to_i}"
     
     return controller.send_data controller.send(:end_of_association_chain).pg_copy_to_string, :filename => "/tmp/#{name}.csv", :type => "application/zip", :disposition => 'inline'
   end
--- lib/postgres-copy/zip_responder.rb
+++ lib/postgres-copy/zip_responder.rb
@@ -3,7 +3,7 @@ Mime::Type.register 'application/zip', :zip
 
 class ActionController::Responder
   def to_zip
-    name = "#{controller.current_client.id}_#{Time.now.to_i}"
+    name = "#{controller.resource.class.name.downcase}_#{Time.now.to_i}"
     controller.send(:end_of_association_chain).pg_copy_to "/tmp/#{name}.csv"
     Dir.chdir('/tmp') do
      `zip #{name} #{name}.csv`
