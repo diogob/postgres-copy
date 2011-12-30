a49eb302f13830d77be1767e9c8da23e380c13fa
Diogo Biazus <diogob@gmail.com>
2011-12-30 14:36:22 -0200

    fixed mime type for the CSV responder

--- lib/postgres-copy/csv_responder.rb
+++ lib/postgres-copy/csv_responder.rb
@@ -2,6 +2,6 @@ class ActionController::Responder
   def to_csv
     name = "#{controller.resource.class.name.downcase}_#{Time.now.to_i}"
     
-    return controller.send_data controller.send(:end_of_association_chain).pg_copy_to_string, :filename => "/tmp/#{name}.csv", :type => "application/zip", :disposition => 'inline'
+    return controller.send_data controller.send(:end_of_association_chain).pg_copy_to_string, :filename => "/tmp/#{name}.csv", :type => "text/csv", :disposition => 'inline'
   end
 end
