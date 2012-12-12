6125899a29f2ec4b245bf74128ac5de8fe9e9dbf
Pete Brumm <pete@petebrumm.com>
2012-12-11 21:42:56 -0600

    Adding ability to import or export binary data instead of csv

--- lib/postgres-copy/active_record.rb
+++ lib/postgres-copy/active_record.rb
@@ -2,19 +2,25 @@ module ActiveRecord
   class Base
     # Copy data to a file passed as a string (the file path) or to lines that are passed to a block
     def self.pg_copy_to path = nil, options = {}
-      options = {:delimiter => ","}.merge(options)
+      options = {:delimiter => ",", :format => :csv, :header => true}.merge(options)
+      options_string = if options[:format] == :binary
+                        "BINARY"
+                       else
+                        "DELIMITER '#{options[:delimiter]}' CSV #{options[:header] ? 'HEADER' : ''}"
+                       end
+
       if path
         raise "You have to choose between exporting to a file or receiving the lines inside a block" if block_given?
-        connection.execute "COPY (#{self.scoped.to_sql}) TO #{sanitize(path)} WITH DELIMITER '#{options[:delimiter]}' CSV HEADER"
+        connection.execute "COPY (#{self.scoped.to_sql}) TO #{sanitize(path)} WITH #{options_string}"
       else
-        connection.execute "COPY (#{self.scoped.to_sql}) TO STDOUT WITH DELIMITER '#{options[:delimiter]}' CSV HEADER"
+        connection.execute "COPY (#{self.scoped.to_sql}) TO STDOUT WITH #{options_string}"
         while line = connection.raw_connection.get_copy_data do
           yield(line) if block_given?
         end
       end
       return self
     end
-    
+
     # Copy all data to a single string
     def self.pg_copy_to_string options = {}
       data = ''
@@ -27,21 +33,44 @@ module ActiveRecord
     # * You can map fields from the file to different fields in the table using a map in the options hash
     # * For further details on usage take a look at the README.md
     def self.pg_copy_from path_or_io, options = {}
-      options = {:delimiter => ","}.merge(options)
+      options = {:delimiter => ",", :format => :csv}.merge(options)
+      options_string = if options[:format] == :binary
+                        "BINARY"
+                       else
+                        "DELIMITER '#{options[:delimiter]}' CSV"
+                       end
       io = path_or_io.instance_of?(String) ? File.open(path_or_io, 'r') : path_or_io
       # The first line should be always the HEADER.
-      line = io.gets
-      columns_list = options[:columns] || line.strip.split(options[:delimiter])
+      if options[:format] == :binary
+        columns_list = options[:columns] || []
+      else
+        line = io.gets
+        columns_list = options[:columns] || line.strip.split(options[:delimiter])
+      end
+
       columns_list = columns_list.map{|c| options[:map][c.to_s] } if options[:map]
-      connection.execute %{COPY #{quoted_table_name} ("#{columns_list.join('","')}") FROM STDIN WITH DELIMITER '#{options[:delimiter]}' CSV}
-      while line = io.gets do
-        next if line.strip.size == 0
-        if block_given?
-          row = line.strip.split(options[:delimiter])
-          yield(row)
-          line = row.join(options[:delimiter]) + "\n"
+      columns_string = columns_list.size > 0 ? "(\"#{columns_list.join('","')}\")" : ""
+      puts %{COPY #{quoted_table_name} ("#{columns_list.join('","')}") FROM STDIN WITH #{options_string}}
+      connection.execute %{COPY #{quoted_table_name} #{columns_string} FROM STDIN WITH #{options_string}}
+      if options[:format] == :binary
+        bytes = 0
+        begin
+          while line = io.readpartial(10240)
+            connection.raw_connection.put_copy_data line
+            bytes += line.bytesize
+          end
+        rescue EOFError
         end
-        connection.raw_connection.put_copy_data line
+      else
+        while line = io.gets do
+          next if line.strip.size == 0
+          if block_given?
+            row = line.strip.split(options[:delimiter])
+            yield(row)
+            line = row.join(options[:delimiter]) + "\n"
+          end
+          connection.raw_connection.put_copy_data line
+        end
       end
       connection.raw_connection.put_copy_end
     end
---
+++ spec/pg_copy_to_binary_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "COPY TO" do
+  before(:all) do
+    ActiveRecord::Base.connection.execute %{
+      TRUNCATE TABLE test_models;
+      SELECT setval('test_models_id_seq', 1, false);
+}
+    TestModel.create :data => 'text'
+  end
+
+  describe "should allow binary output to string" do
+    context "with only binary option" do
+      subject{ TestModel.pg_copy_to_string(:format => :binary).force_encoding("ASCII-8BIT") }
+      it{ should == File.open('spec/fixtures/2_col_binary_data.dat', 'r:ASCII-8BIT').read }
+    end
+    context "with custom select" do
+      subject{ TestModel.select("id, data").pg_copy_to_string(:format => :binary).force_encoding("ASCII-8BIT") }
+      it{ should == File.open('spec/fixtures/2_col_binary_data.dat', 'r:ASCII-8BIT').read }
+    end
+  end
+
+  describe "should allow binary output to file" do
+    it "should copy to disk if block is not given and a path is passed" do
+      TestModel.pg_copy_to '/tmp/export.dat', :format => :binary
+      str = File.open('/tmp/export.dat', 'r:ASCII-8BIT').read
+
+      str.should == File.open('spec/fixtures/2_col_binary_data.dat', 'r:ASCII-8BIT').read
+      
+    end
+  end
+
+end
