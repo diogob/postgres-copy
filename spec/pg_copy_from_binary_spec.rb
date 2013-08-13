41afec70f5c6b2431f045388b14a80d194329a20
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-08-13 15:53:03 -0500

    Clean up deprecations for Rails 4

--- spec/pg_copy_from_binary_spec.rb
+++ spec/pg_copy_from_binary_spec.rb
@@ -10,12 +10,12 @@ describe "COPY FROM BINARY" do
 
   it "should import from file if path is passed without field_map" do
     TestModel.pg_copy_from File.expand_path('spec/fixtures/2_col_binary_data.dat'), :format => :binary, columns: [:id, :data]
-    TestModel.order(:id).all.map{|r| r.attributes}.should == [{'id' => 1, 'data' => 'text'}]
+    TestModel.order(:id).map{|r| r.attributes}.should == [{'id' => 1, 'data' => 'text'}]
   end
 
   it "should import from file if columns are not specified" do
     TestModel.pg_copy_from File.expand_path('spec/fixtures/2_col_binary_data.dat'), :format => :binary
-    TestModel.order(:id).all.map{|r| r.attributes}.should == [{'id' => 1, 'data' => 'text'}]
+    TestModel.order(:id).map{|r| r.attributes}.should == [{'id' => 1, 'data' => 'text'}]
   end
 
 end
