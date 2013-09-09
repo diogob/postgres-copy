31df363a3f9aecd1c94057ba816640ceb566fe77
Diogo Biazus <diogob@gmail.com>
2013-09-09 16:14:07 -0300

    fixed CsvResponder

--- lib/postgres-copy/csv_responder.rb
+++ lib/postgres-copy/csv_responder.rb
@@ -2,7 +2,7 @@ module Responders
   module CsvResponder
     def to_csv
       controller.response_body = Enumerator.new do |y|
-        controller.send(:end_of_association_chain).pg_copy_to do |line|
+        controller.send(:end_of_association_chain).copy_to do |line|
           y << line
         end
       end
