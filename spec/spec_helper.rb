8174a8f1f8bd17298f2a6eefd657669687a52531
Ricardo Castañeda <ricardojudo@gmail.com>
2015-02-13 12:05:57 -0600

    CSV library based implementation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'rspec'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
+  
   config.before(:suite) do
     # we create a test database if it does not exist
     # I do not use database users or password for the tests, using ident authentication instead
@@ -24,6 +25,7 @@ RSpec.configure do |config|
       ActiveRecord::Base.connection.execute %{
         SET client_min_messages TO warning;
         DROP TABLE IF EXISTS test_models;
+        DROP TABLE IF EXISTS test_extended_models; 
         DROP TABLE IF EXISTS extra_fields;
         DROP TABLE IF EXISTS reserved_word_models;
         CREATE TABLE test_models (id serial PRIMARY KEY, data text);
